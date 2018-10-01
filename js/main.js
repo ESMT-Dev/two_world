f5e38cee6a6697d5c32b0e01d660fde2da925b70
Galina <C:\Users\User\AppData\Roaming\The Bat!>
2018-10-01 21:18:10 +0300

    header and section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-{\rtf1}
+$(window).scroll(function(){
+	var st = $(this).scrollTop();
+	$(".parText").css({
+		"transform":"translate(0%, -"+ st/40  + "%"
+	})
+	});
+
+$(window).scroll(function(){
+	var st = $(this).scrollTop();
+	$(".polygon, .polygon_tree").css({
+		"transform":"translate(0%, -"+ st/20  + "%"
+	})
+	});
